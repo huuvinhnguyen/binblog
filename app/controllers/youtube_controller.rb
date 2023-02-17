3015564c96fce45ff3e6b2eb1820ca3cc478c83d
Vinh Nguyen <huuvinh@live.com>
2023-02-17 13:57:44 +0700

    update controller

--- app/controllers/youtube_controller.rb
+++ app/controllers/youtube_controller.rb
@@ -8,22 +8,22 @@ class YoutubeController < ApplicationController
     video_url = params[:video_url]
 
     if video_url.present? 
-      # # Tải xuống video YouTube dưới dạng tệp MP4
-      # video = YoutubeDL::Video.new(video_url, { format: 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' })
-      # video_path = video.download
-      #
-      # # Chuyển đổi tệp MP4 thành tệp MP3
-      # movie = FFMPEG::Movie.new(video_path)
-      # mp3_path = "#{Rails.root}/tmp/#{SecureRandom.hex}.mp3"
-      # movie.transcode(mp3_path, audio_codec: 'libmp3lame')
-      #
-      # # Gửi tệp MP3 dưới dạng tải xuống
-      # send_file mp3_path, type: 'audio/mpeg', disposition: 'attachment', filename: "#{video.title}.mp3"
-      #
-      # # Xóa các tệp tạm thời
-      # File.delete(video_path)
-      # File.delete(mp3_path)    
-      #
+      # Tải xuống video YouTube dưới dạng tệp MP4
+      video = YoutubeDL::Video.new(video_url, { format: 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' })
+      video_path = video.download
+
+      # Chuyển đổi tệp MP4 thành tệp MP3
+      movie = FFMPEG::Movie.new(video_path)
+      mp3_path = "#{Rails.root}/tmp/#{SecureRandom.hex}.mp3"
+      movie.transcode(mp3_path, audio_codec: 'libmp3lame')
+
+      # Gửi tệp MP3 dưới dạng tải xuống
+      send_file mp3_path, type: 'audio/mpeg', disposition: 'attachment', filename: "#{video.title}.mp3"
+
+      # Xóa các tệp tạm thời
+      File.delete(video_path)
+      File.delete(mp3_path)    
+
     end
 
 
