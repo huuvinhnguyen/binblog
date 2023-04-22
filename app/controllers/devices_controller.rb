6d29b21d54fc41f0d27152058d3f15192c9562ed
Bin Nguyen <nguyen.huu.vinh@moneyforward.vn>
2023-04-22 18:18:45 +0700

    - update mqtt and layout of devices

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,36 +1,33 @@
 
 class DevicesController < ApplicationController
+  before_action :initialize_mqtt_client
+  def initialize_mqtt_client
+    @client = MQTT::Client.connect(
+      host: 'a2eoz3l3pmara3-ats.iot.ap-southeast-1.amazonaws.com',
+      port: 8883,
+      ssl: true,
+      cert_file: path_to('cert.crt'),
+      key_file: path_to('private.key'),
+      ca_file: path_to('rootCA.pem'),
+      keep_alive: 30
+    )
+
+  end
 
   def index
-    # MqttWorker.perform_async('topic1')
+  end
 
-    Thread.new do
-      client = MQTT::Client.connect(
-        host: 'a2eoz3l3pmara3-ats.iot.ap-southeast-1.amazonaws.com',
-        port: 8883,
-        ssl: true,
-        cert_file: path_to('cert.crt'),
-        key_file: path_to('private.key'),
-        ca_file: path_to('rootCA.pem'),
-        client_id: 'myClientID',
-        keep_alive: 30
-      )
-
-      client.get('topic', timeout: 2) do |topic, message|
-
-        # json_parse = JSON.parse(message)
-        # puts "#{topic}: #{json_parse}"
-        json_message = JSON.generate(message)
-        ActionCable.server.broadcast('mqtt_channel', json_message)
-
-      end
-      client.disconnect()
-    end
+  def connect
+    session[:device_id] = params[:deviceid]
+    topic = params[:deviceid]
+    subscribe_topic topic
   end
 
   def publish
 
-    topic = params[:topic]
+    puts "publish mess: #{@mess_mqtt}"
+
+    topic = session[:device_id]
     message = params[:message]
 
     client = MQTT::Client.connect(
@@ -43,19 +40,13 @@ class DevicesController < ApplicationController
       # client_id: 'myClientID'
     )
 
-    client.publish(topic, "{\"message\": \"#{message}\"}") if topic.present?
+    client.publish(topic, message, retain: true) if topic.present?
     client.disconnect()
-    redirect_to devices_index_path
-  end
 
-  def latest_data message
-    puts "message: #{message}"
-    render json: { abc: message} if message.present?
-  end
+    subscribe_topic topic
 
-  def mqtt_data
-    render json: { mqtt_data: @queue.pop }
   end
+ 
 
   def new
   end
@@ -73,6 +64,23 @@ class DevicesController < ApplicationController
   end
 
   private
+
+  def subscribe_topic topic
+    Thread.new do
+      @client.subscribe(topic)
+      @client.get(topic, timeout: 2) do |rs_topic, message|
+
+        json_message = JSON.generate(message)
+        ActionCable.server.broadcast('mqtt_channel', json_message)
+        @client.disconnect()
+        ActionCable.server.remote_connections.where(current_user: User.find(1)).disconnect
+
+      end
+      @client.disconnect()
+      ActionCable.server.remote_connections.where(current_user: User.find(1)).disconnect
+    end
+
+  end
 
   def path_to(filename)
     Rails.root.join('config', filename).to_s
--- app/javascript/channels/mqtt_channel.js
+++ app/javascript/channels/mqtt_channel.js
@@ -8,14 +8,32 @@ consumer.subscriptions.create("MqttChannel", {
 
   disconnected() {
     console.log("Disconnected from MQTT channel")
+    this.unsubscribe();
   },
 
   received(data) {
 
-    const mess1 = String(data);
-    const dict = JSON.parse(mess1)
-    console.log("Received MQTT data1111:", message)
-    const mqttMessages = document.querySelector("#mqtt-messages")
-    mqttMessages.insertAdjacentHTML("beforeend",`<p>${mess1}</p>`)
+    const string_data = JSON.parse(data.replace(/(?:\\[rn])+/g, ''));
+    const message_hash = JSON.parse(string_data.replace(/(?:\\[rn])+/g, ''));
+
+    console.log(typeof message_hash);
+    console.log("message_hash:", message_hash)
+    console.log("message_hash.value", message_hash.value)
+    let status;
+
+    var switchDiv = document.getElementById("switch-div");
+    switchDiv.style.display = "block";
+    if (message_hash.value == 1) {
+      status = "Đang hoạt động";
+      document.getElementById('message_field').value = '{ "value": 0 }';
+
+    } else {
+      status = "không hoạt động";
+      document.getElementById('message_field').value = '{ "value": 1 }';
+
+    }
+    const status_element = document.querySelector("#device-status");
+    status_element.textContent = status;
+
   }
 })
