e2020637f3be19e48bc710e7a2c9b2bfaa138b85
Vinh Nguyen <huuvinh@live.com>
2024-10-18 15:17:47 +0700

    [Oct 18] update time zone to VN

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module RubyGettingStarted
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Set the default time zone to Vietnam
+    config.time_zone = 'Asia/Ho_Chi_Minh'
+    
+    # Ensure Active Record stores times in the database in UTC, but uses local time elsewhere
+    config.active_record.default_timezone = :local
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
