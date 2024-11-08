38fa40c47557a34644ec6acdb6b4e8b0fb8a3206
Vinh Nguyen <huuvinh@live.com>
2024-11-08 14:32:39 +0700

    [Nov 8] test print message

--- app/controllers/api/devices_controller.rb
+++ app/controllers/api/devices_controller.rb
@@ -4,7 +4,7 @@ module Api
     def receive_info
       # Nhận và xử lý dữ liệu từ ESP8266
     message = params.permit(:device_type, :topic_type, :device_id, :switch_value, :update_at, :longlast, :timetrigger, reminder: {}, reminders: [])
-    
+    puts "message receive: #{message}"
     ActionCable.server.broadcast('mqtt_channel', message)
 
     render json: { status: 'success', message: 'Device information received' }, status: :ok
