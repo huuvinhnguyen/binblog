ca682e7bfaadd2da8ec5bc81ce51f49b55392e7e
Vinh Nguyen <huuvinh@live.com>
2024-12-09 07:39:32 +0700

    [Dec 9 2024] change email to user login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_08_134302) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_08_164605) do
   create_table "attendances", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.bigint "employee_id"
     t.date "date"
@@ -123,8 +123,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_08_134302) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "webhook_url"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   create_table "users_roles", id: false, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
