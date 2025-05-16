25ffa36b7111e9ae7109950973f61dcc4eab39e5
Vinh Nguyen <huuvinh@live.com>
2025-05-16 23:35:41 +0700

    [May 16 2025] add refresh when running job

--- app/worker/reminder_cron_worker.rb
+++ app/worker/reminder_cron_worker.rb
@@ -16,14 +16,37 @@ class ReminderCronWorker
         
         if next_time.present? && next_time.between?(now - 5.minutes, future_time)
           reminder.schedule_next_job!
-
+          refresh(reminder.device.chip_id)
         end
 
         # Tắt relay
         if off_time.present? && off_time.between?(now - 5.minutes, now + 5.minutes)
           reminder.schedule_turn_off_job!
+          refresh(reminder.device.chip_id)
         end
       end
     end
+
+    private
+
+  def refresh(chip_id)
+    topic = "#{chip_id}/refresh"
+    client = mqtt_client
+
+    message = {
+      action: "refresh",
+      sent_time: Time.current.strftime('%Y-%m-%d %H:%M:%S')
+    }.to_json
+
+    client.publish(topic, message) if topic.present?
+    client.disconnect
   end
+
+  def mqtt_client
+    MQTT::Client.connect(
+      host: "103.9.77.155",
+      port: 1883
+    )
+  end
+end
   
