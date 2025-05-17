ac091a3a097b044095ea2b237defb278c56d7d6d
Vinh Nguyen <huuvinh@live.com>
2025-05-17 23:04:33 +0700

    [May 17 2025] add logs and reset wifi api

--- app/worker/reminder_cron_worker.rb
+++ app/worker/reminder_cron_worker.rb
@@ -11,35 +11,93 @@ class ReminderCronWorker
   
         next_time = reminder.next_trigger_time
         off_time  = reminder.turn_off_time
+        
   
         # Bật relay
-        
         if next_time.present? && next_time.between?(now - 5.minutes, future_time)
           reminder.schedule_next_job!
-          refresh(reminder.device.chip_id)
+          user_id = current_user&.id rescue nil
+
+          log = RelayLog.create(
+              device_id: reminder.device.id,
+              relay_index: reminder.relay_index,
+              turn_on_at: next_time,
+              turn_off_at: nil,
+              triggered_by: "ReminderCronWorker",
+              command_source: "reminder",
+              user_id: user_id,
+              note: "Set relay ON trong #{(reminder.duration / 1_000)} giây qua Reminder"
+            )
+
+          unless log.persisted?
+            Rails.logger.error("RelayLog creation failed: #{log.errors.full_messages.join(', ')}")
+          end
+          refresh(reminder.device.chip_id, log.id)
+
         end
 
         # Tắt relay
         if off_time.present? && off_time.between?(now - 5.minutes, now + 5.minutes)
           reminder.schedule_turn_off_job!
-          refresh(reminder.device.chip_id)
+          user_id = current_user&.id rescue nil
+
+          log = RelayLog.create(
+              device_id: reminder.device.id,
+              relay_index: reminder.relay_index,
+              turn_on_at: nil,
+              turn_off_at: off_time,
+              triggered_by: "ReminderCronWorker",
+              command_source: "reminder",
+              user_id: user_id,
+              note: "Set relay ON trong #{(reminder.duration / 1_000)} giây qua Reminder"
+            )
+
+          unless log.persisted?
+            Rails.logger.error("RelayLog creation failed: #{log.errors.full_messages.join(', ')}")
+          end
+          refresh(reminder.device.chip_id, log.id)
+
         end
+
       end
     end
 
     private
 
-  def refresh(chip_id)
+    def refresh_device(reminder)
+
+      user_id = current_user&.id rescue nil
+
+        log = RelayLog.create(
+            device_id: reminder.device.id,
+            relay_index: reminder.relay_index,
+            turn_on_at: next_time,
+            turn_off_at: off_time,
+            triggered_by: "ReminderCronWorker",
+            command_source: "reminder",
+            user_id: user_id,
+            note: "Set relay ON trong #{(reminder.duration / 1_000)} giây qua Reminder"
+          )
+
+        unless log.persisted?
+          Rails.logger.error("RelayLog creation failed: #{log.errors.full_messages.join(', ')}")
+        end
+        refresh(reminder.device.chip_id, log.id)
+
+    end
+
+  def refresh(chip_id, log_id = nil)
     topic = "#{chip_id}/refresh"
+
     client = mqtt_client
-
     message = {
-      action: "refresh",
-      sent_time: Time.current.strftime('%Y-%m-%d %H:%M:%S')
-    }.to_json
+        "action": "refresh",
+        "sent_time": Time.current.strftime('%Y-%m-%d %H:%M:%S')
+     }
 
-    client.publish(topic, message) if topic.present?
-    client.disconnect
+    message[:log_id] = log_id if log_id.present?
+    client.publish(topic, message.to_json) if topic.present?
+    client.disconnect()
   end
 
   def mqtt_client
