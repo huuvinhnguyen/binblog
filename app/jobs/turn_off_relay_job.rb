a97683b4de2a19c618a36d085cb0208099d2a08d
Vinh Nguyen <huuvinh@live.com>
2025-05-19 11:04:22 +0700

    [May 19 2025] refactor log relays

--- app/jobs/turn_off_relay_job.rb
+++ app/jobs/turn_off_relay_job.rb
@@ -1,7 +1,8 @@
 class TurnOffRelayJob
     include Sidekiq::Worker
   
-    def perform(device_id, relay_index)
+    def perform(device_id, relay_index, reminder_id = nil)
+
       device = Device.find_by(chip_id: device_id)
       return unless device&.device_info.present?
   
@@ -17,6 +18,68 @@ class TurnOffRelayJob
       device.update(device_info: device_info.to_json)
   
       puts "TurnOffRelayJob: Đã tắt relay #{relay_index} của thiết bị #{device_id}"
+
+       
+
+      reminder = Reminder.find_by(id: reminder_id)
+      if reminder.present? 
+
+        turn_off_at = Time.current
+        note = "Set relay ON trong #{(reminder.duration / 1_000)} giây qua Reminder"
+        log = create_log(device_id, relay_index, reminder.turn_on_at, reminder.turn_off_at, note)
+        refresh(device_id, log.id)
+      
+
+      else
+
+        turn_off_at = Time.current
+        log = create_log(device_id, relay_index, turn_off_at)
+        refresh(device_id, log&.id)
+        note = "Turn off relay"
+
+      end
+      
+    end
+
+    private
+    def create_log(device_id, relay_index, turn_on_at = nil, turn_off_at = nil, duration = nil, note = nil)
+
+      log = RelayLog.create(
+                device_id: device_id,
+                relay_index: relay_index,
+                turn_on_at: turn_on_at,
+                turn_off_at: turn_off_at,
+                triggered_by: "ReminderCronWorker",
+                command_source: "reminder",
+                user_id: nil,
+                note: note
+              )
+  
+      unless log.persisted?
+        Rails.logger.error("RelayLog creation failed: #{log.errors.full_messages.join(', ')}")
+      end
+      log
+    end
+
+    def refresh(chip_id, log_id = nil)
+      topic = "#{chip_id}/refresh"
+  
+      client = mqtt_client
+      message = {
+          "action": "refresh",
+          "sent_time": Time.current.strftime('%Y-%m-%d %H:%M:%S')
+       }
+  
+      message[:log_id] = log_id if log_id.present?
+      client.publish(topic, message.to_json) if topic.present?
+      client.disconnect()
+    end
+  
+    def mqtt_client
+      MQTT::Client.connect(
+        host: "103.9.77.155",
+        port: 1883
+      )
     end
 
 end
--- app/worker/reminder_cron_worker.rb
+++ app/worker/reminder_cron_worker.rb
@@ -21,21 +21,21 @@ class ReminderCronWorker
           reminder.schedule_next_job!
           puts "schedule_next_job "
 
-          log = RelayLog.create(
-              device_id: reminder.device.id,
-              relay_index: reminder.relay_index,
-              turn_on_at: next_time,
-              turn_off_at: nil,
-              triggered_by: "ReminderCronWorker",
-              command_source: "reminder",
-              user_id: nil,
-              note: "Set relay ON trong #{(reminder.duration / 1_000)} giây qua Reminder"
-            )
+          # log = RelayLog.create(
+          #     device_id: reminder.device.id,
+          #     relay_index: reminder.relay_index,
+          #     turn_on_at: next_time,
+          #     turn_off_at: nil,
+          #     triggered_by: "ReminderCronWorker",
+          #     command_source: "reminder",
+          #     user_id: nil,
+          #     note: "Set relay ON trong #{(reminder.duration / 1_000)} giây qua Reminder"
+          #   )
 
-          unless log.persisted?
-            Rails.logger.error("RelayLog creation failed: #{log.errors.full_messages.join(', ')}")
-          end
-          refresh(reminder.device.chip_id, log.id)
+          # unless log.persisted?
+          #   Rails.logger.error("RelayLog creation failed: #{log.errors.full_messages.join(', ')}")
+          # end
+          # refresh(reminder.device.chip_id, log.id)
 
         end
 
@@ -43,48 +43,48 @@ class ReminderCronWorker
         if off_time.present? && off_time.between?(now - 5.minutes, now + 5.minutes)
           reminder.schedule_turn_off_job!
 
-          log = RelayLog.create(
-              device_id: reminder.device.id,
-              relay_index: reminder.relay_index,
-              turn_on_at: nil,
-              turn_off_at: off_time,
-              triggered_by: "ReminderCronWorker",
-              command_source: "reminder",
-              user_id: nil,
-              note: "Set relay ON trong #{(reminder.duration / 1_000)} giây qua Reminder"
-            )
+          # log = RelayLog.create(
+          #     device_id: reminder.device.id,
+          #     relay_index: reminder.relay_index,
+          #     turn_on_at: nil,
+          #     turn_off_at: off_time,
+          #     triggered_by: "ReminderCronWorker",
+          #     command_source: "reminder",
+          #     user_id: nil,
+          #     note: "Set relay ON trong #{(reminder.duration / 1_000)} giây qua Reminder"
+          #   )
 
-          unless log.persisted?
-            Rails.logger.error("RelayLog creation failed: #{log.errors.full_messages.join(', ')}")
-          end
-          refresh(reminder.device.chip_id, log.id)
+          # unless log.persisted?
+          #   Rails.logger.error("RelayLog creation failed: #{log.errors.full_messages.join(', ')}")
+          # end
+          # refresh(reminder.device.chip_id, log.id)
 
         end
 
       end
     end
 
-    private
+    # private
 
-  def refresh(chip_id, log_id = nil)
-    topic = "#{chip_id}/refresh"
+  # def refresh(chip_id, log_id = nil)
+  #   topic = "#{chip_id}/refresh"
 
-    client = mqtt_client
-    message = {
-        "action": "refresh",
-        "sent_time": Time.current.strftime('%Y-%m-%d %H:%M:%S')
-     }
+  #   client = mqtt_client
+  #   message = {
+  #       "action": "refresh",
+  #       "sent_time": Time.current.strftime('%Y-%m-%d %H:%M:%S')
+  #    }
 
-    message[:log_id] = log_id if log_id.present?
-    client.publish(topic, message.to_json) if topic.present?
-    client.disconnect()
-  end
+  #   message[:log_id] = log_id if log_id.present?
+  #   client.publish(topic, message.to_json) if topic.present?
+  #   client.disconnect()
+  # end
 
-  def mqtt_client
-    MQTT::Client.connect(
-      host: "103.9.77.155",
-      port: 1883
-    )
-  end
+  # def mqtt_client
+  #   MQTT::Client.connect(
+  #     host: "103.9.77.155",
+  #     port: 1883
+  #   )
+  # end
 end
   
