bad21e48b852358db165571c8a75650ae8c34e73
Vinh Nguyen <huuvinh@live.com>
2025-05-25 17:55:20 +0700

    [May 2025 25] refactor code

--- app/worker/reminder_cron_worker.rb
+++ app/worker/reminder_cron_worker.rb
@@ -7,24 +7,23 @@ class ReminderCronWorker
       future_time = now + 5.minutes   
       Reminder.includes(:device).where(enabled: true).find_each do |reminder|
         
-        next unless reminder.start_time.present? && reminder.relay_index.present?
+        next unless reminder.next_trigger_time.present? && reminder.relay_index.present?
   
-        next_time = reminder.next_trigger_time
-        off_time  = reminder.turn_off_time
+        turn_on_at = reminder.next_trigger_time
+        turn_off_at  = reminder.turn_off_time
   
         # Bật relay
         
-        should_trigger = reminder.last_triggered_at.nil? || reminder.last_triggered_at < next_time
-        if should_trigger && next_time.present? && next_time.between?(now - 5.minutes, future_time)
+        should_trigger = reminder.last_triggered_at.nil? || reminder.last_triggered_at < turn_on_at
+        if should_trigger && turn_on_at.between?(now - 5.minutes, future_time)
           reminder.schedule_next_job!
           puts "schedule_next_job "
 
         end
 
         # Tắt relay
-        if off_time.present? && off_time.between?(now - 5.minutes, now + 5.minutes)
+        if turn_off_at.present? && turn_off_at.between?(now - 5.minutes, now + 5.minutes)
           reminder.schedule_turn_off_job!
-
         end
 
       end
