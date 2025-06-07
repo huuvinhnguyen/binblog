034ab1524adc2d244390a49ff63a8a1ca9ab87d2
Vinh Nguyen <huuvinh@live.com>
2025-06-07 11:26:19 +0700

    [June 7 2025] remove vite and using importmap chartjs

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,33 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+import { Chart, registerables } from 'chart.js'
+
+Chart.register(...registerables)
+
+
+document.addEventListener('turbo:load', () => {
+    const ctx = document.getElementById('myChart');
+    if (!ctx) return;
+  
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4'],
+        datasets: [{
+          label: 'Số lượng',
+          data: [12, 19, 3, 5],
+          backgroundColor: ['#3490dc', '#38c172', '#ffed4a', '#e3342f'],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  });
--- config/importmap.rb
+++ config/importmap.rb
@@ -12,4 +12,5 @@ pin "bootstrap", to: 'bootstrap.min.js', preload: true
 pin "gauges", to: "gauges.js", preload: true
 pin "device_switch", to: "device_switch.js", preload: true
 
-
+pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@4.2.0/dist/chart.js"
+pin "@kurkle/color", to: "https://ga.jspm.io/npm:@kurkle/color@0.3.2/dist/color.esm.js"
