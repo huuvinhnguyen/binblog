186308cba2533affcbe57c3007fa6fa2d745fba5
Vinh Nguyen <huuvinh@live.com>
2025-09-30 23:48:21 +0700

    [2025-09-30] add metadata for sharing link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+    def meta_title
+        content_for?(:meta_title) ? content_for(:meta_title) : "KhuônViên"
+    end
+
+    def meta_description
+        content_for?(:meta_description) ? content_for(:meta_description) : "Giải pháp Thiết bị Thông minh cho Nông nghiệp công nghệ cao"
+    end
+
+    def meta_image
+        content_for?(:meta_image) ? content_for(:meta_image) : asset_url("favicon.svg")
+    end
+
+    def meta_url
+        request.original_url
+    end
 end
