8ba0d43028724f23aaf5aa1b1e2f4ebef60522b0
Vinh Nguyen <huuvinh@live.com>
2025-10-05 13:24:15 +0700

    [05-Oct-2025] update fix rails admin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,8 @@ Rails.application.configure do
   config.action_cable.url = "wss://khuonvien.vn/cable"
   config.action_controller.default_url_options = { host: "khuonvien.vn", protocol: "https" }
   config.force_ssl = true
+  Rails.application.config.action_controller.forgery_protection_origin_check = false
+
   config.action_cable.allowed_request_origins = ["http://khuonvien.vn", /http:\/\/khuonvien.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
