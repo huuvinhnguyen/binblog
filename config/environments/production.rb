670cf1a2f9a676ebd18d64a1be655766ab7f2e6c
Vinh Nguyen <huuvinh@live.com>
2025-10-05 13:21:28 +0700

    [05-Oct-2025] force ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
   config.action_cable.url = "wss://khuonvien.vn/cable"
   config.action_controller.default_url_options = { host: "khuonvien.vn", protocol: "https" }
+  config.force_ssl = true
   config.action_cable.allowed_request_origins = ["http://khuonvien.vn", /http:\/\/khuonvien.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
